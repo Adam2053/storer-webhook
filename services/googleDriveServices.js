864f170c0fd52483db5306f07efb4f7aa79198d3
Arpit Marathe <arpitmarathe2002@gmail.com>
2025-08-14 04:17:52 +0530

    webhook-service4

--- services/googleDriveServices.js
+++ services/googleDriveServices.js
@@ -1,5 +1,5 @@
 import { google } from "googleapis";
-import { v4 as uuidv4 } from "uuid";
+import { randomUUID } from "crypto";
 import axios from "axios";
 import logger from "../utils/logger.js";
 import { getGoogleAuthClient } from "../config/googleAuth.js";
@@ -10,6 +10,8 @@ export async function getStartPageToken(auth) {
   return res.data.startPageToken;
 }
 
+const channelId = `arkive-drive-webhook-${randomUUID()}`;
+
 export const registerDriveWebhook = async () => {
   try {
     const auth = await getGoogleAuthClient();
@@ -19,20 +21,23 @@ export const registerDriveWebhook = async () => {
 
     const drive = google.drive({ version: "v3", auth });
 
-    const startToken = (await drive.changes.getStartPageToken()).data.startPageToken;
+    const startToken = (await drive.changes.getStartPageToken()).data
+      .startPageToken;
 
     const res = await drive.changes.watch({
       pageToken: startToken,
       requestBody: {
-        id: "arkive-drive-webhook-12345", // must be unique every time you register
+        id: channelId, // must be unique every time you register
         type: "web_hook",
-        address: process.env.WEBHOOK_URL // your webhook endpoint
-      }
+        address: process.env.WEBHOOK_URL, // your webhook endpoint
+      },
     });
 
     console.log("✅ Webhook registered:", res.data);
   } catch (err) {
-    console.error("❌ Webhook registration failed:", err.response?.data || err.message);
+    console.error(
+      "❌ Webhook registration failed:",
+      err.response?.data || err.message
+    );
   }
 };
-
